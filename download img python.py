3a18276c300595eb5ff7159716fcdf683446c360
kavindukalinga <74819742+kavindukalinga@users.noreply.github.com>
2022-05-31 18:46:25 +0530

    Update and rename download img python.txt to download img python.py

--- download img python.py
+++ download img python.py
@@ -3,10 +3,11 @@ import urllib.request
 
 
 def downimg(url):
-    name=random.randrange(1,1000)
+    name=random.randrange(1,1000) # Giving a random name to the file
     fullname=str(name)+".jpg"
     urllib.request.urlretrieve(url,fullname)
-#begining
+
+# Example
 downimg("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg")
 
 
